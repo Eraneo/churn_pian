dfb30d511c6bb24643c7c7c47d5a6db843a9b04d
Eraneo <elvianeraneo2005@gmail.com>
2026-06-06 01:28:57 +0700

    ganti kategori data keseluruhan

--- ngetest.py
+++ ngetest.py
@@ -5,10 +5,11 @@ import pandas as pd
 # import dataset
 data = pd.read_csv("https://raw.githubusercontent.com/Eraneo/churn_pian/refs/heads/main/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 print (data.info())
+print (data["PaymentMethod"].value_counts())
 
-print (data["TotalCharges"].head())
-# kedepannya kudu labelencoder untuk TotalCharges karena masih string
-print (data)
+# Ganti kategori data
+data_copy = data.copy()
+# data biner
 binary_cols = [
     'gender',
     'Partner',
@@ -19,7 +20,31 @@ binary_cols = [
 ]
 
 for col in binary_cols:
-    data[col] = data[col].map({
+    data_copy[col] = data_copy[col].map({
         'Yes':1, 'No':0,
         'Male':1, 'Female':0
     })
+
+# data kategorikal
+data_copy = pd.get_dummies(
+    data_copy,
+    columns=[
+        'MultipleLines',
+        'InternetService',
+        'OnlineSecurity',
+        'OnlineBackup',
+        'DeviceProtection',
+        'TechSupport',
+        'StreamingTV',
+        'StreamingMovies',
+        'Contract',
+        'PaymentMethod'
+    ],
+    drop_first=True
+)
+
+# data numerik
+data_copy['TotalCharges'] = pd.to_numeric(data_copy['TotalCharges'], errors='coerce')
+
+print (data_copy.info())
+print (data_copy.head())
